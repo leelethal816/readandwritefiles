ef6e8bc6a803146ffd519edc430487bbec279c36
leelethal816 <zhangtianyi816@gmail.com>
2023-01-30 13:10:35 -0600

    Assignment Submission 2

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -14,9 +14,23 @@ sales_report_csv = csv.writer(sales_report, delimiter=",")
 sales_report_csv.writerow(["CustomerID", "Total"])
 
 # Read CustomerID, SubTotal, TaxAmt, and Freight, calculate Total, and write it to salesreport.csv
+total = 0
+customer_id_list = []
+
 for line in sales_csv:
-    total = format((float(line[3]) + float(line[4]) + float(line[5])), ".2f")
-    sales_report_csv.writerow([line[0], total])
+    if customer_id_list == []:
+        customer_id_list.append(line[0])
+
+    if line[0] not in customer_id_list:
+        total = format(total, ".2f")
+        sales_report_csv.writerow([customer_id_list[-1], total])
+        customer_id_list.append(line[0])
+        total = 0
+        total += float(line[3]) + float(line[4]) + float(line[5])
+    elif line[0] in customer_id_list:
+        total += float(line[3]) + float(line[4]) + float(line[5])
+
+sales_report_csv.writerow([customer_id_list[-1], total])
 
 # Close both files
 sales.close()
