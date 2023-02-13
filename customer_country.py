5121445aa45df31ab7d54af0eb55873677d72508
leelethal816 <zhangtianyi816@gmail.com>
2023-02-13 15:31:30 -0600

    Customer Country Correction

--- customer_country.py
+++ customer_country.py
@@ -3,19 +3,20 @@ import csv
 
 # Open customers.csv file and read it as csv file
 infile = open("customers.csv", "r")
-infile_csv = csv.reader(infile, delimiter=",")
+# infile_csv = csv.reader(infile, delimiter=",")
 
 # Create customer_country.csv and add header
 outfile = open("customer_country.csv", "w")
-outfile_csv = csv.writer(outfile, delimiter=",")
-outfile_csv.writerow(["Name", "Country"])
+# outfile_csv = csv.writer(outfile, delimiter=",")
+outfile.write("Name" + "," + "Country" + "\n")
 
 # Skip header for infile_csv
-next(infile_csv)
+next(infile)
 
 # Write First Name, Last Name, and Country from infile_csv to outfile_csv
-for line in infile_csv:
-    outfile_csv.writerow([(line[1] + " " + line[2]), line[4]])
+for line in infile:
+    line_list = line.split(",")
+    outfile.write(line_list[1] + " " + line_list[2] + "," + line_list[4] + "\n")
 
 # Close both files
 infile.close()
